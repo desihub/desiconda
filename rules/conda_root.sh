711e253a2775b49a7c8eb0d7e7c585d285d62010
Theodore Kisner <mail@theodorekisner.com>
2017-02-21 09:36:10 -0800

    Force miniconda to use older python 3.5.2 compatible versions of all packages.

--- rules/conda_root.sh
+++ rules/conda_root.sh
@@ -1,5 +1,6 @@
 curl -SL @MINICONDA@ \
     -o miniconda.sh \
     && /bin/bash miniconda.sh -b -f -p @CONDA_PREFIX@ \
+    && conda install python=3.5.2 \
     && rm miniconda.sh \
     && rm -rf @CONDA_PREFIX@/pkgs/*
