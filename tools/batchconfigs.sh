99643e3f1270658442f666072e716d83c8e89a3f
Theodore Kisner <tskisner.public@gmail.com>
2017-04-13 13:30:20 -0700

    Add helper script to generate multiple install scripts on nersc machines.

---
+++ tools/batchconfigs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This is just a helper script to generate install
+# scripts for a NERSC machine.
+
+ver="$1"
+
+mach="${NERSC_HOST}"
+
+specconfs=""
+survconfs=""
+if [ "${mach}" = "edison" ]; then
+    specconfs="gcc"
+    survconfs="gcc-py27"
+elif [ "${mach}" = "cori" ]; then
+    specconfs="gcc haswell-intel knl-intel"
+    survconfs="gcc-py27"
+fi
+
+# install and module prefix
+
+inst="/global/common/${mach}/contrib/desi/code"
+moddir="/global/common/${mach}/contrib/desi/modulefiles"
+
+for conf in ${survconfs}; do
+    CONFIG="${mach}-${conf}" PREFIX="${inst}" VERSION="${ver}-survey" \
+    MODULEDIR="${moddir}" make survey
+done
+
+for conf in ${specconfs}; do
+    CONFIG="${mach}-${conf}" PREFIX="${inst}" VERSION="${ver}-spectro" \
+    MODULEDIR="${moddir}" make spectro
+done
