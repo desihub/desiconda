45b0c5e9a1a36507ffa3ae78e7eaf8eb41be30d8
Theodore Kisner <tskisner.public@gmail.com>
2017-04-13 13:14:11 -0700

    Only regex replace scons scripts with python2.

--- rules/scons.sh
+++ rules/scons.sh
@@ -5,4 +5,4 @@ curl -SL http://prdownloads.sourceforge.net/scons/scons-2.5.1.tar.gz \
     && python2 setup.py install --prefix="@AUX_PREFIX@" \
     && cd .. \
     && rm -rf scons* \
-    && perl -i -p -e 's/usr\/bin\/env python/usr\/bin\/env python2/g' @AUX_PREFIX@/bin/*
+    && perl -i -p -e 's/usr\/bin\/env python/usr\/bin\/env python2/g' @AUX_PREFIX@/bin/scons*
