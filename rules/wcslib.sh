c9f166d4f9040d8193bece96a059ae3daa5a5692
Theodore Kisner <mail@theodorekisner.com>
2017-04-26 22:19:53 -0700

    Update wcslib build rule to include a patch needed cross compiling.  Add wcslib pkg-config path so that astrometry.net build can find it.

--- rules/wcslib.sh
+++ rules/wcslib.sh
@@ -2,8 +2,15 @@ curl -SL ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib-5.16.tar.bz2 \
     -o wcslib-5.16.tar.bz2 \
     && tar xjf wcslib-5.16.tar.bz2 \
     && cd wcslib-5.16 \
-    && CC="@CC@" CFLAGS="@CFLAGS@" ./configure @CROSS@ \
-    --prefix=@AUX_PREFIX@ \
+    && chmod -R u+w . \
+    && patch -p1 < ../rules/patch_wcslib \
+    && autoconf \
+    && CC="@CC@" CFLAGS="@CFLAGS@" \
+    CPPFLAGS="-I@AUX_PREFIX@/include" \
+    LDFLAGS="-L@AUX_PREFIX@/lib" \
+    ./configure @CROSS@ \
+    --disable-fortran \
+    --prefix="@AUX_PREFIX@" \
     && make -j 4 && make install \
     && cd .. \
     && rm -rf wcslib*
