57492f1f19badccc37b38e7ee1bf6d5c37fe3184
Theodore Kisner <tskisner.public@gmail.com>
2017-06-13 10:12:03 -0700

    Add more conda and pip dependencies.  No need for separate photutils rule since it is pip-installable.

--- rules/conda_pkgs.sh
+++ rules/conda_pkgs.sh
@@ -2,15 +2,18 @@ conda install --copy --yes \
     nose \
     requests \
     future \
+    cython \
     numpy \
     scipy \
     matplotlib \
+    basemap \
     pyyaml \
     astropy \
     hdf5 \
     h5py \
     psutil \
     ephem \
+    psycopg2 \
     numba=0.28.1 \
     && conda install --copy --yes -c defaults \
     ipython ipython-notebook \
