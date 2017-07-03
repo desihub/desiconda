5414a138b5a50012f054f3b7c7797fde577bcd8f
Theodore Kisner <tskisner.public@gmail.com>
2017-07-03 14:17:47 -0700

    Bump version of harp

--- rules/harp.sh
+++ rules/harp.sh
@@ -1,12 +1,11 @@
-curl -SL https://github.com/tskisner/HARP/releases/download/v1.0.3/harp-1.0.3.tar.bz2 \
-    -o harp-1.0.3.tar.bz2 \
-    && tar xjf harp-1.0.3.tar.bz2 \
-    && cd harp-1.0.3 \
+curl -SL https://github.com/tskisner/HARP/releases/download/v1.0.4/harp-1.0.4.tar.bz2 \
+    -o harp-1.0.4.tar.bz2 \
+    && tar xjf harp-1.0.4.tar.bz2 \
+    && cd harp-1.0.4 \
     && CC="@CC@" CXX="@CXX@" CFLAGS="@CFLAGS@" CXXFLAGS="@CXXFLAGS@" ./configure @CROSS@ \
     --disable-mpi --disable-python \
     --with-cfitsio="@AUX_PREFIX@" \
     --with-boost="@AUX_PREFIX@" \
-    --with-blas="@BLAS@" \
     --with-lapack="@LAPACK@" \
     --prefix=@AUX_PREFIX@ \
     && make -j 4 && make install \
