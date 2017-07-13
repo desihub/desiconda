cf6febf82c2a213ec47b395199883445965ba834
Theodore Kisner <tskisner.public@gmail.com>
2017-07-12 19:06:37 -0700

    Add line to tractor build so that it finds ceres.

--- rules/tractor.sh
+++ rules/tractor.sh
@@ -2,6 +2,7 @@ curl -SL https://github.com/dstndstn/tractor/archive/dr5.1.tar.gz \
     -o tractor-dr5.1.tar.gz \
     && tar xzf tractor-dr5.1.tar.gz \
     && cd tractor-dr5.1 \
-    && python setup.py install --with-ceres --prefix="@AUX_PREFIX@" \
+    && CERES_LIB="-L@AUX_PREFIX@/lib -lceres" \
+    python setup.py install --with-ceres --prefix="@AUX_PREFIX@" \
     && cd .. \
     && rm -rf tractor*
