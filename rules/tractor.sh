820b50c26102c6c90bc078405437a97558bf358d
Theodore Kisner <tskisner.public@gmail.com>
2017-07-19 08:15:06 -0700

    Bump tractor to 5.2

--- rules/tractor.sh
+++ rules/tractor.sh
@@ -1,7 +1,7 @@
-curl -SL https://github.com/dstndstn/tractor/archive/dr5.1.tar.gz \
-    -o tractor-dr5.1.tar.gz \
-    && tar xzf tractor-dr5.1.tar.gz \
-    && cd tractor-dr5.1 \
+curl -SL https://github.com/dstndstn/tractor/archive/dr5.2.tar.gz \
+    -o tractor-dr5.2.tar.gz \
+    && tar xzf tractor-dr5.2.tar.gz \
+    && cd tractor-dr5.2 \
     && CERES_LIB="-L@AUX_PREFIX@/lib -lceres" \
     python setup.py install --with-ceres --prefix="@AUX_PREFIX@" \
     && cd .. \
