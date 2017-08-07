99393e044614f8f6e5ad3bb4758147b4dbfd082e
Theodore Kisner <tskisner.public@gmail.com>
2017-08-07 08:42:05 -0700

    Build cfitsio with reentrant support

--- rules/cfitsio.sh
+++ rules/cfitsio.sh
@@ -2,9 +2,10 @@ curl -SL http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3410.tar.gz \
     -o cfitsio3410.tar.gz \
     && tar xzf cfitsio3410.tar.gz \
     && cd cfitsio \
-    && CC="@CC@" CFLAGS="@CFLAGS@" ./configure @CROSS@ --prefix=@AUX_PREFIX@ \
-    && make -j 4 && make shared \
-    && make fpack && make funpack \
+    && CC="@CC@" CFLAGS="@CFLAGS@" ./configure @CROSS@ \
+    --prefix=@AUX_PREFIX@ --enable-reentrant \
+    && make stand_alone utils \ 
+    && make shared \
     && make install \
     && cd .. \
     && rm -rf cfitsio*
