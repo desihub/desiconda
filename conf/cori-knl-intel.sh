9dbae8d9c36d15fff38ef82458353b135e80d0bf
Theodore Kisner <tskisner.public@gmail.com>
2017-08-07 08:57:25 -0700

    Update toolchain versions on NERSC configs.

--- conf/cori-knl-intel.sh
+++ conf/cori-knl-intel.sh
@@ -11,8 +11,8 @@ if [ "x${loadedintel}" = x ]; then
     fi
 fi
 module swap craype-haswell craype-mic-knl
-module swap intel intel/17.0.1.132
-module load gcc/6.2.0
+module swap intel intel/17.0.2.174
+module load gcc/6.3.0
 module load cmake
 module load git
 export CRAYPE_LINK_TYPE=dynamic
