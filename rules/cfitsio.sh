f77cfea541f10fce626d5dd35d12bee0e4d4ef7f
Theodore Kisner <tskisner.public@gmail.com>
2017-08-10 06:10:35 -0700

    Fix whitespace typo

--- rules/cfitsio.sh
+++ rules/cfitsio.sh
@@ -5,7 +5,7 @@ curl -SL http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3410.tar.gz \
     && CC="@CC@" CFLAGS="@CFLAGS@" ./configure @CROSS@ \
     --prefix=@AUX_PREFIX@ --enable-reentrant \
     && make stand_alone \
-    && make utils \ 
+    && make utils \
     && make shared \
     && make install \
     && cd .. \
