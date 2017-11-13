dc6b7b1e90ef9c769171e2d8f189f39ab1889dac
Theodore Kisner <tskisner.public@gmail.com>
2017-11-12 22:02:14 -0800

    Update boost config.  Fix tmv blas linking.

--- rules/tmv.sh
+++ rules/tmv.sh
@@ -5,8 +5,8 @@ curl -SL https://github.com/rmjarvis/tmv/archive/v0.74.tar.gz \
     && scons PREFIX="@AUX_PREFIX@" \
     CXX="@CXX@" FLAGS="@CXXFLAGS@" \
     EXTRA_INCLUDE_PATH="@BLAS_INCLUDE@" \
-    EXTRA_LIB_PATH=$(echo @BLAS@ | sed -e 's#-L\([^ ]*\).*#\1#') \
-    LIBS="$(echo @BLAS@ | sed -e 's#-L\([^ ]*\)##' | sed -e 's#-l\([^ ]*\)#\1#g' | sed -e 's#-[^ ]*##g')" \
+    EXTRA_LIB_PATH=$(echo @BLAS@ | sed -e 's#-[^L]\S\+##g' | sed -e 's#-L\(\S\+\).*#\1#g') \
+    LIBS="$(echo @BLAS@ | sed -e 's#-[^l]\S\+##g' | sed -e 's#-l\(\S\+\)#\1#g')" \
     FORCE_FBLAS=true \
     && scons PREFIX="@AUX_PREFIX@" install \
     && cd .. \
