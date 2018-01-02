7aee319f8fd9c190088d275815ccebde4ae51be3
Theodore Kisner <tskisner.public@gmail.com>
2018-01-02 06:05:22 -0800

    Small tweaks to tractor build to use Intel compilers.

--- rules/tractor.sh
+++ rules/tractor.sh
@@ -1,9 +1,10 @@
 curl -SL https://github.com/dstndstn/tractor/archive/dr6.1.tar.gz \
     -o tractor-dr6.1.tar.gz \
     && tar xzf tractor-dr6.1.tar.gz \
-    && cd tractor-dr6.1 \
-    && CERES_LIB="-L@AUX_PREFIX@/lib -lceres" \
+    && cd tractor-dr6.1 && patch -p1 < ../rules/patch_tractor \
+    && CERES_LIB_DIR="@AUX_PREFIX@/lib" \
     CC="@CC@" CFLAGS="@CFLAGS@" LDSHARED="@CC@ -shared" \
+    CXX="@CXX@" CXXFLAGS="@CXXFLAGS@" BLAS_LIB="@BLAS@" \
     python setup.py install --with-ceres --prefix="@AUX_PREFIX@" \
     && cd .. \
     && rm -rf tractor*
