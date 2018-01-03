842bc5c32c5113eb076e8c5f51f8e3f3f4d9b8aa
Theodore Kisner <tskisner.public@gmail.com>
2018-01-03 12:48:07 -0800

    Fix healpix download path

--- rules/healpix.sh
+++ rules/healpix.sh
@@ -1,6 +1,5 @@
-curl -SL https://github.com/tskisner/healpix-autotools/releases/download/v3.31.4/healpix-autotools-3.31.4.tar.gz \
-    -o healpix-autotools-3.31.4.tar.gz \
-    && tar -xzf healpix-autotools-3.31.4.tar.gz \
+curl -SL https://github.com/tskisner/healpix-autotools/releases/download/v3.31.4/healpix-autotools-3.31.4.tar.bz2 \
+    | tar -xjf - \
     && cd healpix-autotools-3.31.4 \
     && CC="@CC@" CXX="@CXX@" FC="@FC@" \
     CFLAGS="@CFLAGS@" CXXFLAGS="@CXXFLAGS@" FCFLAGS="@FCFLAGS@" \
