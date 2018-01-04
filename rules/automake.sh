b58905ac805e7d4d5828d6061343184dc47bb36b
Theodore Kisner <tskisner.public@gmail.com>
2018-01-03 22:43:56 -0800

    Bump automake version to fix bug with newer perl.

--- rules/automake.sh
+++ rules/automake.sh
@@ -1,7 +1,7 @@
-curl -SL https://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz \
-    -o automake-1.15.tar.gz \
-    && tar xzf automake-1.15.tar.gz \
-    && cd automake-1.15 \
+curl -SL https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.gz \
+    -o automake-1.15.1.tar.gz \
+    && tar xzf automake-1.15.1.tar.gz \
+    && cd automake-1.15.1 \
     && CC="gcc" ./configure \
     --prefix="@AUX_PREFIX@" \
     && make -j 4 && make install \
