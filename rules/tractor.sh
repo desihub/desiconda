86a8dee834aca6b35ce0110c620566dd822fcf8f
Stephen Bailey <StephenBailey@lbl.gov>
2018-05-11 10:51:01 -0700

    update to astrometry.net/0.7 and tractor/dr7.0

--- rules/tractor.sh
+++ rules/tractor.sh
@@ -1,7 +1,7 @@
-curl -SL https://github.com/dstndstn/tractor/archive/dr6.2.tar.gz \
-    -o tractor-dr6.2.tar.gz \
-    && tar xzf tractor-dr6.2.tar.gz \
-    && cd tractor-dr6.2 && patch -p1 < ../rules/patch_tractor \
+curl -SL https://github.com/dstndstn/tractor/archive/dr7.0.tar.gz \
+    -o tractor-dr7.0.tar.gz \
+    && tar xzf tractor-dr7.0.tar.gz \
+    && cd tractor-dr7.0 && patch -p1 < ../rules/patch_tractor \
     && CERES_LIB_DIR="@AUX_PREFIX@/lib" \
     CC="@CC@" CFLAGS="@CFLAGS@" LDSHARED="@CC@ -shared" \
     CXX="@CXX@" CXXFLAGS="@CXXFLAGS@" BLAS_LIB="@BLAS@" \
