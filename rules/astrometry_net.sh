d47a5b3bb3a9614c953c58464451d942c6cc68c2
Stephen Bailey <StephenBailey@lbl.gov>
2018-05-11 10:51:01 -0700

    update to astrometry.net/0.7 and tractor/dr7.0

--- rules/astrometry_net.sh
+++ rules/astrometry_net.sh
@@ -1,6 +1,6 @@
-curl -SL https://github.com/dstndstn/astrometry.net/archive/0.73.tar.gz -o astrometry.net-0.73.tar.gz \
-    && tar xzf astrometry.net-0.73.tar.gz \
-    && cd astrometry.net-0.73 \
+curl -SL https://github.com/dstndstn/astrometry.net/archive/0.74.tar.gz -o astrometry.net-0.74.tar.gz \
+    && tar xzf astrometry.net-0.74.tar.gz \
+    && cd astrometry.net-0.74 \
     && CC="@CC@" CXX="@CXX@" CFLAGS="@CFLAGS@" CXXFLAGS="@CXXFLAGS@" \
     LDFLAGS="-L@AUX_PREFIX@/lib -lz" LDSHARED="@CC@ -shared" \
     WCSLIB_INC="-I@AUX_PREFIX@/include/wcslib" \
