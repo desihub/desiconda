6d8fafa99db095941fbfcec7f65eb04edc4de2cd
Theodore Kisner <tskisner.public@gmail.com>
2018-08-21 07:42:47 -0700

    Upstream healpy installation (which includes a compiled extension) now works on all machines of interest.  Get this with pip now rather than the autotools bundle.

--- rules/pip_pkgs.sh
+++ rules/pip_pkgs.sh
@@ -2,6 +2,7 @@ pip install --no-binary :all: \
     speclite \
     hpsspy \
     photutils \
+    healpy \
     coveralls \
     line_profiler \
     https://github.com/esheldon/fitsio/archive/v0.9.12rc1.zip
