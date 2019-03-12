f094c8adb3931c3479d057afa842352a50c488fc
Theodore Kisner <tskisner.public@gmail.com>
2019-03-12 09:50:58 -0700

    Fix building of tractor against ceres and eigen3

--- rules/tractor.sh
+++ rules/tractor.sh
@@ -2,8 +2,9 @@ curl -SL https://github.com/dstndstn/tractor/archive/dr8.0.tar.gz \
     -o tractor-dr8.0.tar.gz \
     && tar xzf tractor-dr8.0.tar.gz \
     && cd tractor-dr8.0 \
-    && CERES_LIB_DIR="@AUX_PREFIX@/lib" \
-    CC="@CC@" CFLAGS="@CFLAGS@" LDSHARED="@CC@ -shared" \
+    && CERES_LIB="-lceres" \
+    CERES_LIB_DIR="@AUX_PREFIX@/lib" \
+    CC="@CC@" CFLAGS="@CFLAGS@ -I@AUX_PREFIX@/include/eigen3" LDSHARED="@CC@ -shared" \
     CXX="@CXX@" CXXFLAGS="@CXXFLAGS@" BLAS_LIB="@BLAS@" \
     python setup.py install --with-ceres --prefix="@AUX_PREFIX@" \
     && cd .. \
