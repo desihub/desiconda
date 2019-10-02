e909b8a8e54ee1e19f889eecb8306cc8b822aa2e
Theodore Kisner <tskisner.public@gmail.com>
2019-10-01 23:50:59 -0700

    Bump openblas to 0.3.7, which has correct detection of ancient compilers like gcc-4.8, found at KPNO.

--- rules/openblas.sh
+++ rules/openblas.sh
@@ -1,6 +1,6 @@
-curl -SL https://github.com/xianyi/OpenBLAS/archive/v0.3.5.tar.gz \
+curl -SL https://github.com/xianyi/OpenBLAS/archive/v0.3.7.tar.gz \
     | tar xzf - \
-    && cd OpenBLAS-0.3.5 \
+    && cd OpenBLAS-0.3.7 \
     && make USE_OPENMP=1 NO_SHARED=0 \
     FC="@FC@" FCOMMON_OPT="@FCFLAGS@" \
     CC="@CC@" COMMON_OPT="@CFLAGS@" \
