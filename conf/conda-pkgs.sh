8676e4259b841bc48577967940a58b9b6c788789
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2022-01-11 12:18:23 -0800

    finish reversion

---
+++ conf/conda-pkgs.sh
@@ -0,0 +1,69 @@
+# conda packages
+echo Current time $(date) Installing conda packages
+echo condadir is $CONDADIR
+
+conda install --copy --yes -c conda-forge \
+    astropy \
+    speclite \
+    fitsio \
+    fitsverify \
+    libblas=*=*mkl \
+    dask \
+    distributed \
+    papermill \
+    nose \
+    requests \
+    future \
+    cython \
+    cmake \
+    numpy \
+    scipy \
+    intel-openmp \
+    mkl=2020.0 \
+    matplotlib \
+    seaborn \
+    pyyaml \
+    pytest-astropy \
+    hdf5 \
+    h5py \
+    psutil \
+    ephem \
+    psycopg2 \
+    pytest \
+    pytest-cov \
+    numba \
+    sqlalchemy \
+    scikit-learn \
+    scikit-image \
+    ipython \
+    jupyter \
+    ipywidgets \
+    bokeh \
+    wurlitzer \
+    certipy \
+    sphinx \
+    iminuit \
+    cudatoolkit \
+    cupy \
+    healpy \
+    photutils \
+    xlrd \
+    coveralls \
+    configobj \
+    cupy \
+    line_profiler \
+    galsim \
+    altair \
+    vega_datasets \
+&& mplrc="$CONDADIR/lib/python$PYVERSION/site-packages/matplotlib/mpl-data/matplotlibrc"; \
+    cat ${mplrc} | sed -e "s#^backend.*#backend : TkAgg#" > ${mplrc}.tmp; \
+    mv ${mplrc}.tmp ${mplrc} \
+&& rm -rf $CONDADIR/pkgs/*
+
+if [ $? != 0 ]; then
+    echo "ERROR installing conda packages; exiting"
+    exit 1
+fi
+
+conda list --export | grep -v conda > "$CONDADIR/pkg_list.txt"
+echo Current time $(date) Done installing conda packages
---
+++ conf/pip-pkgs.sh
@@ -0,0 +1,15 @@
+# Install pip packages.
+echo Installing pip packages at $(date)
+
+pip install --no-binary :all: hpsspy
+pip install threadpoolctl
+
+# see https://docs.nersc.gov/development/languages/python/parallel-python/
+pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
+
+if [ $? != 0 ]; then
+    echo "ERROR installing pip packages; exiting"
+    exit 1
+fi
+
+echo Current time $(date) Done installing conda packages
