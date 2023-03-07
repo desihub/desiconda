110e8f840dd250e6edcc4b211315306f923ed3f5
dli-astro <ananda.li@gmail.com>
2023-03-07 08:19:05 -0700

    Do not install pip packages on desi-7.

--- conf/pip-pkgs.sh
+++ conf/pip-pkgs.sh
@@ -1,15 +1,17 @@
 # Install pip packages.
-echo Installing pip packages at $(date)
+if [ "$HOSTNAME" != "desi-7" ]; then
+    echo Installing pip packages at $(date)
 
-pip install --no-binary :all: hpsspy
-pip install threadpoolctl
+    pip install --no-binary :all: hpsspy
+    pip install threadpoolctl
 
-# see https://docs.nersc.gov/development/languages/python/parallel-python/
-pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
+    # see https://docs.nersc.gov/development/languages/python/parallel-python/
+    pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
 
-if [ $? != 0 ]; then
-    echo "ERROR installing pip packages; exiting"
-    exit 1
+    if [ $? != 0 ]; then
+        echo "ERROR installing pip packages; exiting"
+        exit 1
+    fi
+
+    echo Current time $(date) Done installing conda packages
 fi
-
-echo Current time $(date) Done installing conda packages
