f13a755f358fdd627f1f83f2aaf25141ef658bbd
Segev BenZvi <sybenzvi@users.noreply.github.com>
2025-03-20 17:09:58 -0400

    Force a match astropy used at NERSC.

--- conf/conda-pkgs.sh
+++ conf/conda-pkgs.sh
@@ -14,7 +14,7 @@ echo condadir is $CONDADIR
 #   https://github.com/conda-forge/ucx-split-feedstock/issues/172
 
 conda install --copy --yes -c conda-forge \
-    "astropy<7.0" \
+    "astropy<6.1" \
     fitsio \
     fitsverify \
     libblas=*=*mkl \
