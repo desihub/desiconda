199465415cfbd2da03e8d003d28c44f607b9405b
desiNERSC <StephenBailey@lbl.gov>
2026-03-24 20:07:05 -0700

    updates for 26.3/matterhorn

--- conf/conda-pkgs.sh
+++ conf/conda-pkgs.sh
@@ -5,19 +5,29 @@ echo condadir is $CONDADIR
 # Notes:
 # - cupy-core instead of cupy so that it won't install any cuda libraries,
 #   which we will get from NERSC cudatoolkit module instead
-# - mkl=2020.0 because that is the last version that guarantees bitwise
+# - OLD: mkl=2020.0 because that is the last version that guarantees bitwise
 #   identical output for bitwise idential input
-# - bokeh<3 because prospect and nightwatch don't yet support bokeh 3
-# - numpy<2.0 because we haven't tested with numpy 2.x yet.
+# - bokeh<3 because nightwatch doesn't yet support bokeh 3
 # - ucx constraint is to avoid bringing in cuda libraries due to mal-formed
 #   dependencies "dask -> pyarrow -> libarrow -> ucx"
 #   https://github.com/conda-forge/ucx-split-feedstock/issues/172
+#
+# - changes on 2025-02-20:
+#   - removed numpy<2 pin
+#   - removed mkl=2020.0 pin (alas, more recent versions don't guarantee
+#     bitwise reproducibility even on identical inputs)
+#   - Move to openblas+openmp instead of mkl
+#   - Added numba-cuda in addition to numba (future-proofing)
+#   - Added pytest-xdist for pytest parallelism
+#   - Added setuptools-scm to support desiInstall speclite and specsim (desiutil #227)
 
 conda install --copy --yes -c conda-forge \
     astropy \
     fitsio \
     fitsverify \
-    libblas=*=*mkl \
+    "libblas=*=*_openblas" \
+    "libopenblas=*=*openmp*" \
+    "openblas=*=*openmp*" \
     dask \
     "ucx=1.14.1=*_0" \
     distributed \
@@ -27,10 +37,9 @@ conda install --copy --yes -c conda-forge \
     future \
     cython \
     cmake \
-    "numpy<2.0" \
+    numpy \
     scipy \
     intel-openmp \
-    mkl=2020.0 \
     matplotlib \
     seaborn \
     pyyaml \
@@ -42,7 +51,9 @@ conda install --copy --yes -c conda-forge \
     psycopg2 \
     pytest \
     pytest-cov \
+    pytest-xdist \
     numba \
+    numba-cuda \
     sqlalchemy \
     scikit-learn \
     scikit-image \
@@ -69,6 +80,7 @@ conda install --copy --yes -c conda-forge \
     altair \
     vega_datasets \
     conda-tree \
+    setuptools-scm \
 && mplrc="$CONDADIR/lib/python$PYVERSION/site-packages/matplotlib/mpl-data/matplotlibrc"; \
     cat ${mplrc} | sed -e "s#^backend.*#backend : TkAgg#" > ${mplrc}.tmp; \
     mv ${mplrc}.tmp ${mplrc} \
