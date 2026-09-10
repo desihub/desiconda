fa77fa74d035cfee59c17e3f606cc696ea25c096
Stephen Bailey <StephenBailey@lbl.gov>
2026-09-10 09:45:09 -0700

    Fail fast if any pip install fails in pip-pkgs.sh
    Previously only the last pip install's exit code was checked, so an
    earlier failure (e.g. hpsspy or the CUDA package group) could be
    masked and the script would still report success.

--- conf/pip-pkgs.sh
+++ conf/pip-pkgs.sh
@@ -1,5 +1,7 @@
 # Install pip packages.
 echo Installing pip packages at $(date)
+set -e
+trap 'echo "ERROR installing pip packages (line $LINENO); exiting"; exit 1' ERR
 
 #- SB 2024-03-22: previous hpsspy install method fails, but basic install works
 # pip install --no-binary :all: hpsspy
@@ -12,10 +14,5 @@ pip install threadpoolctl
 
 # see https://docs.nersc.gov/development/languages/python/parallel-python/
 pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
-
-if [ $? != 0 ]; then
-    echo "ERROR installing pip packages; exiting"
-    exit 1
-fi
 
 echo Current time $(date) Done installing pip packages
