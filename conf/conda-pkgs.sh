c56182cd565c2f117263eadc591d95bec56d8b79
Segev BenZvi <segev.benzvi@rochester.edu>
2026-09-15 11:14:52 -0400

    Do not restrict astropy version.

--- conf/conda-pkgs.sh
+++ conf/conda-pkgs.sh
@@ -24,7 +24,7 @@ echo condadir is $CONDADIR
 #   - Added ipympl for interactive plotting in jupyter
 
 conda install --copy --yes -c conda-forge \
-    "astropy<6.1" \
+    astropy \
     fitsio \
     fitsverify \
     "libblas=*=*_openblas" \
